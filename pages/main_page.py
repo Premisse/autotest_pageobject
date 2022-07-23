27856dcad4481506cc997ba5c5a0a72804cb4739
Premisse <premisse@yandex.ru>
2022-07-23 23:29:41 +0300

    create test of current url

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
-from .locators import MainPageLocators, LoginPageLocators
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
@@ -12,13 +11,5 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-
-    def should_be_login_url(self):
-        assert "login" in self.current_url(), "'login' is not in current page url"
 
 
