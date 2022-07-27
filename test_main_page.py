3c94a4fe16bb2f48bcebfdd6e1615c879146c0a0
Premisse <premisse@yandex.ru>
2022-07-28 00:50:52 +0300

    added mark need_reviw

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,3 @@
-import time
-
 import pytest
 
 from .pages.main_page import MainPage
@@ -23,32 +21,23 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()
 
-    def test_guest_can_login(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-        page = LoginPage(browser, link)
-        page.open()
-        email = str(time.time()) + "@fakemail.org"
-        password = "111111111"
-        page.register_new_user(email, password)
-        page.user_is_registered()
-#
-# def test_guest_should_see_login_form(browser):
-#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-#     page = LoginPage(browser, link)
-#     page.open()
-#     page.should_be_login_form()
-#
-# def test_guest_should_see_register_form(browser):
-#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-#     page = LoginPage(browser, link)
-#     page.open()
-#     page.should_be_register_form()
-#
-# def test_current_url_contains_login(browser):
-#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-#     page = LoginPage(browser, link)
-#     page.open()
-#     page.should_be_login_url()
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+def test_current_url_contains_login(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/"
